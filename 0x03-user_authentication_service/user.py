add932f11484420e7b976bf56874fb45cc7f92f2
gekwealor <gekwealor@gmail.com>
2024-03-25 07:42:27 -0400

    User authenticate

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    User model for the 'users' table.
+    """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self) -> str:
+        """
+        String representation of the User instance.
+        """
+        return f"User: id={self.id}"
